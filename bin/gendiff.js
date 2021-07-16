5d7e87d54b20e23ce88db5e3c91deec3d945e826
FreeS <frees.comp@gmail.com>
2021-07-16 13:26:39 +0300

    Add GitHub Action

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
-import program from 'commander';
-import { genDiff } from '../src/index.js'
 import path from 'path';
+import program from 'commander';
+import genDiff from '../src/index.js';
+
 program
-    .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0')
-    .option('-f, --format [type] ', 'Output format')
-    .arguments('<filepath1> <filepath2>')
-    .action((firstFilePath, secondFilePath) => (
-        console.log(genDiff(path.resolve(process.cwd(), firstFilePath), path.resolve(process.cwd(), secondFilePath)))))
-    .parse(process.agrv);
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .option('-f, --format [type] ', 'Output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((firstFilePath, secondFilePath) => (
+    console.log(genDiff(path.resolve(process.cwd(), firstFilePath), path.resolve(process.cwd(), secondFilePath)))))
+  .parse(process.agrv);
--- src/index.js
+++ src/index.js
@@ -7,30 +7,28 @@ const isSharedKey = (key, obj1, obj2) => _.has(obj1, key) && _.has(obj2, key);
 const hasSameValue = (key, obj1, obj2) => obj1[key] === obj2[key];
 const toObj = (data) => JSON.parse(data);
 
-export const genDiff = (pathFile1, pathFile2) => {
-
-const data1 = fs.readFileSync(`${pathFile1}`, {encoding:'utf8', flag:'r'});
-const data2 = fs.readFileSync(`${pathFile2}`, {encoding:'utf8', flag:'r'});
-
-const obj1 = toObj(data1);
-const obj2 = toObj(data2);
+export default (pathFile1, pathFile2) => {
+  const data1 = fs.readFileSync(`${pathFile1}`, { encoding: 'utf8', flag: 'r' });
+  const data2 = fs.readFileSync(`${pathFile2}`, { encoding: 'utf8', flag: 'r' });
+  const obj1 = toObj(data1);
+  const obj2 = toObj(data2);
   const int = '  ';
   const keys = _.union(_.keys(obj1), _.keys(obj2)).sort();
-  const result = keys.reduce((res, key) =>{
+  const result = keys.reduce((res, key) => {
     let newRes = '';
     if (isDeletedKey(key, obj1, obj2)) {
-      newRes = res + int +`- ${key}: ${obj1[key]}\n`;
+      newRes = `${res}${int}- ${key}: ${obj1[key]}\n`;
     }
     if (isSharedKey(key, obj1, obj2) && hasSameValue(key, obj1, obj2)) {
-      newRes = res + int + `  ${key}: ${obj1[key]}\n`;    
+      newRes = `${res}${int}  ${key}: ${obj1[key]}\n`;
     }
     if (isSharedKey(key, obj1, obj2) && !hasSameValue(key, obj1, obj2)) {
-      newRes = res + int + `- ${key}: ${obj1[key]}` + '\n' + int + `+ ${key}: ${obj2[key]}\n`; 
+      newRes = `${res}${int}- ${key}: ${obj1[key]}\n${int}+ ${key}: ${obj2[key]}\n`;
     }
     if (isNewKey(key, obj1, obj2)) {
-      newRes = res + int + `+ ${key}: ${obj2[key]}\n`;
+      newRes = `${res}${int}+ ${key}: ${obj2[key]}\n`;
     }
     return newRes;
   }, '');
-  return '{\n' + result + '}';
+  return `{\n${result}}`;
 };
